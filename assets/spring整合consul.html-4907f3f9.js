59dc1e4811cca921b7c88256288df676eb163579
SparksDing <36055521+SparksDing@users.noreply.github.com>
2024-04-07 02:18:21 +0000

    Deploying to gh-pages from @ SparksDing/SparksDing.github.io@e7fc799d9090871eda2d262544cdc1534571980a 🚀

---
+++ assets/spring整合consul.html-4907f3f9.js
@@ -0,0 +1,8 @@
+import{_ as t}from"./plugin-vue_export-helper-c27b6911.js";import{r as s,o,c as i,d as e,e as r,b as a,f as l}from"./app-03183e8e.js";const c={},d=l(`<h3 id="安装" tabindex="-1"><a class="header-anchor" href="#安装" aria-hidden="true">#</a> 安装</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>brew tap hashicorp/tap
+brew install hashicorp/tap/consul
+
+To start hashicorp/tap/consul now and restart at login:
+  brew services start hashicorp/tap/consul
+Or, if you don&#39;t want/need a background service you can just run:
+  /usr/local/opt/consul/bin/consul agent -dev -bind 127.0.0.1
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="参考文章" tabindex="-1"><a class="header-anchor" href="#参考文章" aria-hidden="true">#</a> 参考文章</h3>`,3),u={href:"https://kingfree.gitbook.io/consul/getting-started/install",target:"_blank",rel:"noopener noreferrer"},h={href:"https://blog.csdn.net/qq_37493888/article/details/124850972",target:"_blank",rel:"noopener noreferrer"},p={href:"https://blog.csdn.net/qq_38658567/article/details/112478643",target:"_blank",rel:"noopener noreferrer"};function _(b,v){const n=s("ExternalLinkIcon");return o(),i("div",null,[d,e("p",null,[e("a",u,[r("安装consul"),a(n)])]),e("p",null,[e("a",h,[r("spring集成consul"),a(n)])]),e("p",null,[e("a",p,[r("consul源码分析"),a(n)])])])}const g=t(c,[["render",_],["__file","spring整合consul.html.vue"]]);export{g as default};
